0bb697f57268ca4075581e9b10d35948f5ca4af1
websamrus <websamrus@gmail.com>
2019-11-13 18:37:39 +0200

    feat: fix2 section slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
-$(function(){
+$(function() {
 
-$('.slider__inner').slick({
-    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        // asNavFor: '.slider__mini',
+        dots: true,
+        infinite: false,
+        focusOnSelect: true,
+    });
+
+    // $('.slider__mini').slick({
+    //     slidesToShow: 6,
+    //     // slidesToScroll: 0,
+    //     dots: false,
+    //     arrows: false,
+    //     asNavFor: '.slider__inner',
+    //     centerMode: true,
+    //     focusOnSelect: true,
+    //     infinite: false,
+    // });
+
 });
 
 
-});
+
+
+// $('.slider-for').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.slider-nav'
+// });
+// $('.slider-nav').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     asNavFor: '.slider-for',
+//     dots: true,
+//     centerMode: true,
+//     focusOnSelect: true
+// });
