50f8bb7bbdcd3b2e2f9180464a81f2ad138d41a6
websamrus <websamrus@gmail.com>
2019-11-15 00:33:32 +0200

    feat: add section feature

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
 $(function() {
-
-    // $('.slider__inner').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    //     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    //     asNavFor: '.slider__mini',
-    //     // dots: true,
-    //     fade: true,
-
-    // });
-
-    // $('.slider__mini').slick({
-    //     slidesToShow: 6,
-    //     slidesToScroll: 1,
-    //     dots: false,
-    //     arrows: false,
-    //     asNavFor: '.slider__inner',
-    //     centerMode: true,
-    //     focusOnSelect: true,
-    //     infinite: false,
-    // });
-
     $('.slider__inner').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -30,28 +7,17 @@ $(function() {
         asNavFor: '.slider__mini',
         centerMode: true,
         speed: 1000,
-        //focusOnSelect: true,
-        //infinite: true,
-        //adaptiveHeight: true,
-
-        //useTransform: true,
-        //infinite: true,
     });
+
     $('.slider__mini').slick({
-        slidesToShow: 5,
         slidesToScroll: 1,
-
-
         asNavFor: '.slider__inner',
         arrows: false,
         dots: false,
-        //centerMode: true,
         variableWidth: true,
-
         focusOnSelect: true,
         infinite: true,
-        speed: 1200,
-
+        speed: 1000,
     });
 
 });
